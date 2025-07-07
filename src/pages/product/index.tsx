c74b6a009177188bd1438a7ef3572f9cec4abb24
Rifandiysf <rifandiyusuf47@gmail.com>
2025-07-07 08:36:22 +0700

    remove: validation login

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -3,14 +3,8 @@ import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
 
 const ProductPage = () => {
-    const [isLogin, setIsLogin] = useState(false)
     const [products, setProducts] = useState([])
     const { push } = useRouter()
-    useEffect(() => {
-        if (!!isLogin) {
-            push("/auth/login")
-        }
-    }, [])
 
     useEffect(() => {
         fetch("/api/product").then((res) => res.json().then((response) => {
