4c344d7110ec06f06d1c2bc79e8916b6295f8ef4
Rifandiysf <rifandiyusuf47@gmail.com>
2025-07-07 08:35:16 +0700

    feat: Rendering Dynamic Routes

--- src/pages/product/[product].tsx
+++ src/pages/product/[product].tsx
@@ -1,13 +1,87 @@
+import DetailProduct from "@/components/views/detail"
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 
-const DetailProductPage = () => {
-    const {query} = useRouter()
+type ProductType = {
+    id: string,
+    name: string,
+    image: string,
+    price: number,
+    size: string,
+    category: string
+}
+
+const DetailProductPage = (/*{ product }: { product: ProductType }*/) => {
+    const { query } = useRouter()
+    // Client Side Rendering
+    const [product, setProduct] = useState<ProductType | null>(null)
+
+
+    useEffect(() => {
+        fetch(`/api/product/${query.product}`).then((res) => res.json().then((response) => {
+            setProduct(response.data)
+        }))
+    }, [query.product])
     return (
         <>
-            <h1>Detail Product Page</h1>
-            <p>Product : {query.product}</p>
+            {/* Client Side Rendering */}
+            <h1 style={{ textAlign: "center" }}>Detail Product Page</h1>
+            {product ? (
+                <DetailProduct product={product} />
+            ) : (
+                <p style={{ textAlign: "center" }}>Loading product...</p>
+            )}
+
+            {/* Server Side Rendering */}
+            {/* <h1 style={{ textAlign: "center" }}>Detail Product Page</h1>
+            {product ? (
+                <DetailProduct product={product} />
+            ) : (
+                <p style={{ textAlign: "center" }}>Loading product...</p>
+            )} */}
+
+            {/* Static Side Generation */}
+            {/* <h1 style={{ textAlign: "center" }}>Detail Product Page</h1>
+            {product ? (
+                <DetailProduct product={product} />
+            ) : (
+                <p style={{ textAlign: "center" }}>Loading product...</p>
+            )} */}
         </>
     )
 }
 
 export default DetailProductPage
+
+// Server Side Rendering
+// export async function getServerSideProps({ params, }: { params: { product: string } }) {
+//     const res = await fetch(`http://localhost:3000/api/product/${params.product}`)
+//     const response = await res.json()
+//     return {
+//         props: {
+//             product: response.data
+//         }
+//     }
+// }
+
+// Static Side Generation
+// export async function getStaticPaths() {
+//     const res = await fetch("http://localhost:3000/api/product/")
+//     const response = await res.json()
+//     const paths = response.data.map((product: ProductType) => ({
+//         params: {
+//             product: product.id
+//         }
+//     }))
+//     return {paths, fallback: false}
+// }
+
+// export async function getStaticProps({ params, }: { params: { product: string } }) {
+//     const res = await fetch(`http://localhost:3000/api/product/${params.product}`)
+//     const response = await res.json()
+//     return {
+//         props: {
+//             product: response.data
+//         }
+//     }
+// }
