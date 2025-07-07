c13b5181d8ad0b691269df66fc05d4dd0e6ed0b7
Rifandiysf <rifandiyusuf47@gmail.com>
2025-07-07 08:29:38 +0700

    feat: query for data product

---
+++ src/pages/api/[[...product]].ts
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { retrieveData, retrieveDataById } from '@/utils/database/service'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+    status: boolean,
+    statusCode: number,
+    data: any
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    if (req.query.product[1]) {
+        const data = await retrieveDataById("product", req.query.product[1])
+        res.status(200).json({ status: true, statusCode: 200, data })
+    }
+    const data = await retrieveData()
+    res.status(200).json({ status: true, statusCode: 200, data })
+}
